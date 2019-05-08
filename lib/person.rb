142763e3f8709828cb3400d71a9a8958af3194db
Dalya Eidda <dalia.eidda@gmail.com>
2019-05-08 22:29:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,10 @@ class Person
  attr_accessor :name, :birthday, :hair_color, :eye_color, :height
  
  def initialize(attributes)
-   attributes.each {|key, value| key.send((#{key}),value)}
+   attributes.each {|key, value| self.send((#{key}),value)}
  end 
  
+ def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
